36c0e231604e14587d2dbc6c868ed511000db077
dexteryy <dexter.yy@gmail.com>
2016-10-19 11:14:12 +0800

    feat: webpack 0.5.9; https://github.com/dexteryy/webcube/issues/2

--- configs/webpack.config.babel.js
+++ configs/webpack.config.babel.js
@@ -1,8 +1,20 @@
 
 module.exports = {
 
+  resolveAlias: {},
+
+  babelLoaderPresets: (presets) => {
+    return presets;
+  },
+
+  babelLoaderPlugins: (plugins) => {
+    return plugins;
+  },
+
+  postcssPlugins: [],
+
   plugins: [],
 
-  customFields: [],
+  customFields: {},
 
 };

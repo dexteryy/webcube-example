4fc8f7d8d9b4eda4129dc3ecb111a99ddd848a07
dexteryy <dexter.yy@gmail.com>
2016-11-18 16:41:25 +0800

    feat: webcube 0.5.17; add example for WEBCUBE_DEV_ENTRY_*

--- app/entries/example-app/tests/index.spec.js
+++ app/entries/example-app/tests/index.spec.js
@@ -1,3 +1,4 @@
+/* eslint max-nested-callbacks: 0 */
 
 import { expect } from 'chai';
 import Nightmare from 'nightmare';
@@ -8,16 +9,23 @@ describe('example-app', function () {
 
   this.timeout(60000);
   const rootUrl = util.getUrlRoot();
-  const url = `${rootUrl}/example-app/index.html`;
+  const url = `${rootUrl}/example-app/tests/index.html`;
 
   describe('Start page', function () {
 
     it('should be mounted', function (done) {
+      if (util.isProductionEnv) {
+        this.skip();
+        return;
+      }
       const nightmare = Nightmare({
         show: false,
       });
-      nightmare.goto(url).evaluate(function () {
-        return document.querySelectorAll('#exampleAppRoot').length;
+      nightmare.goto(url).evaluate(function (done) {
+        window._runApp(() => {
+          const n = document.querySelectorAll('#exampleAppRoot').length;
+          done(null, n);
+        });
       }).end().then(result => {
         expect(result).to.be.equal(1);
         done();
